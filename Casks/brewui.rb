d41f128fbb9a2b96270c259c0b3a90abe9339a0b
Nishant Patil <nishant.apatil3@gmail.com>
2026-03-01 23:31:29 -0800

    Bump brewui cask to v0.1.4

--- Casks/brewui.rb
+++ Casks/brewui.rb
@@ -1,6 +1,6 @@
 cask "brewui" do
-  version "0.1.2"
-  sha256 "e28b8b2a24929269b723130e0e567d84d95795ab714f303247e3d23e3bc44442"
+  version "0.1.4"
+  sha256 "e56bedb091389ccc67e0ada11f341e279409af20deab14692a49d4e1fb26ba96"
 
   url "https://github.com/nishantapatil3/brewUI/releases/download/v#{version}/brewUI.app.zip"
   name "brewUI"
